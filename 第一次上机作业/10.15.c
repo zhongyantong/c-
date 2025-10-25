89230f429a2ebde2b40652c9dff63586b55d1732
zhongyantong <3979690785@qq.com>
2025-10-25 18:21:01 +0800

    Add files via upload

---
+++ 第一次上机作业/10.15.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{printf("*********************\n");
+ printf("    very good!\n");
+ printf("*********************\n");
+ return 0;
+}
---
+++ 第一次上机作业/8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,max;
+    scanf("%d%d%d",&a,&b,&c);
+    max=a;
+    if(b>max)max=b;
+    if(c>max)max=c;
+    printf("最大值为：%d\n",max);
+    return 0;
+}
